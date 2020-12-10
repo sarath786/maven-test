1b3a0121ca1543f2a7ae0698768ab040a4663bac
root <root@ip-10-1-0-19.ec2.internal>
2020-12-10 06:43:53 +0000

    test project1

---
+++ src/main/java/com/sarath/org/App.java
@@ -0,0 +1,13 @@
+package com.sarath.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
